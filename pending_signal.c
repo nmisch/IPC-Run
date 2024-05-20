6cb661fa8d341b31dcf740dcc4cae5e9c5244466
Noah Misch <nmisch@google.com>
2024-05-19 17:02:13 -0700

    execv w/ unexpected linux behavior

--- pending_signal.c
+++ pending_signal.c
@@ -7,12 +7,31 @@ int main(int argc, char **argv)
     sigset_t set;
     char msg[] = "reached write()\n";
 
-    sigemptyset(&set);
-    sigaddset(&set, SIGTERM);
-    sigprocmask(SIG_BLOCK, &set, NULL);
+    if (argc == 1)
+    {
+	char *cmd[] = { argv[0], "anything", (char *)0 };
 
-    raise(SIGTERM);
-    write(2, msg, sizeof(msg) - 1);
-    sigprocmask(SIG_UNBLOCK, &set, NULL);
-    return 0;
+	sigemptyset(&set);
+	sigaddset(&set, SIGTERM);
+	sigprocmask(SIG_BLOCK, &set, NULL);
+
+	execv(argv[0], cmd);
+	return 1;
+    }
+    else
+    {
+	/*
+	 * Dies to SIGTERM about half the time, returning 0 the other half.
+	 * Compiling with -DREBLOCK makes it die to SIGTERM always.
+	 */
+#if REBLOCK
+	sigemptyset(&set);
+	sigaddset(&set, SIGTERM);
+	sigprocmask(SIG_BLOCK, &set, NULL);
+#endif
+	raise(SIGTERM);
+	write(2, msg, sizeof(msg) - 1);
+	sigprocmask(SIG_UNBLOCK, &set, NULL);
+	return 0;
+    }
 }
